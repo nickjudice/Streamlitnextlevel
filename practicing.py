445115dd7174ddb887a26d94391eff897792e57a
nickjudice <121053184+nickjudice@users.noreply.github.com>
2023-03-03 08:46:36 -0600

    Update practicing.py

--- practicing.py
+++ practicing.py
@@ -8,5 +8,3 @@ st.title("Streamlit 101: An in-depth introduction")
 st.markdown("Welcome to this in-depth introduction to learning streamlit")
 st.header("Nick's practice")
 st.markdown("One day I'll be a wizard at this.")
-
-st.dataframe(df.head())
