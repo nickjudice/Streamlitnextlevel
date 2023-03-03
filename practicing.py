02375228c67f9a5daa0546f70210ba8e26b506e5
nickjudice <121053184+nickjudice@users.noreply.github.com>
2023-03-03 08:46:28 -0600

    Update practicing.py

--- practicing.py
+++ practicing.py
@@ -3,12 +3,6 @@ import pandas as pd
 import streamlit as st
 
 
-@st.cache_data
-def get_data():
-    url = "http://data.insideairbnb.com/united-states/ny/new-york-city/2019-09-12/visualisations/listings.csv"
-    return pd.read_csv(url)
-
-df = get_data()
 
 st.title("Streamlit 101: An in-depth introduction")
 st.markdown("Welcome to this in-depth introduction to learning streamlit")
