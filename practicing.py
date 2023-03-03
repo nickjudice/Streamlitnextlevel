71afb49baf1b0f15fc2ac7de52f4f82aa64403b0
nickjudice <121053184+nickjudice@users.noreply.github.com>
2023-03-03 09:05:25 -0600

    Update practicing.py

--- practicing.py
+++ practicing.py
@@ -1,25 +1,36 @@
 #Creating new streamlit practice session
-import pandas as pd
 import streamlit as st
+import pandas as pd
 import numpy as np
 
-
-st.title("Streamlit 101: An in-depth introduction")
-st.markdown("Welcome to this in-depth introduction to learning streamlit")
-st.header("Nick's practice")
-st.markdown("One day I'll be a wizard at this.")
+DATA_URL = 'https://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz'
 
 def main():
-  
-  if __name__ == '__main__':
-    main()
-    
-@st.cache_data
-def data_load(nrows):
-  df = pd.read_csv(DATA_URL, nrows=nrows)
-  lower_str = lambda x: str(x).lower()
-  df.rename(lower_str, axis='columns', inplace=True)
-  df['date/time'] = pd.to_datetime(df['date/time'])
-  return df
+ 
+  st.title('Dashboard with UBER data')
 
-df = data_load(1000)
+  @st.cache
+  def data_load(nrows):
+    df = pd.read_csv(DATA_URL, nrows=nrows)
+    lower_str = lambda x: str(x).lower()
+    df.rename(lower_str, axis='columns', inplace=True)
+    df['date/time'] = pd.to_datetime(df['date/time'])
+    return df
+  
+  df = data_load(1000)
+
+  if st.sidebar.checkbox('Show first thousand lines'):
+    st.success('The first thousand lines were loaded')
+    st.write(df)
+  
+  st.subheader('Number of runs per hour')
+  hist = np.histogram(df['date/time'].dt.hour, bins=24, range=(0,24))[0]
+  st.bar_chart(hist)
+
+  hour_filter = st.slider('hour', 0,23,1)
+  f_data = df[df['date/time'].dt.hour == hour_filter]
+  st.subheader('Map:')
+  st.map(f_data)
+
+if __name__ == '__main__':
+  main()
